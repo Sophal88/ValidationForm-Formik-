252954597336e6b4f61233c8ad38cb951f9f8774
Sophal88 <sophalrupp@gmail.com>
2019-10-31 15:59:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import ValidatedLoginForm from './components/Validate';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <center> <h1>Email Login</h1></center>
+      < ValidatedLoginForm  />
+    </React.Fragment>
+  
   );
 }
-
 export default App;
